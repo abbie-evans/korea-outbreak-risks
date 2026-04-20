8c082fa52a93482d368d7d6c954e4fe82c97dff4
Abbie Evans <147088150+abbie-evans@users.noreply.github.com>
2026-04-20 21:31:48 +0100

    Add script for Figure 5

--- model_nigeria.py
+++ model_nigeria.py
@@ -58,12 +58,12 @@ def calculate_contact_matrix(contact_matrix, population):
 
 def system_of_equations(q, beta):
     """
-    System of equations for the local outbreak probability.
+    System of equations for the major outbreak probability.
 
     Parameters
     -----------
     q : numpy.ndarray
-        Probability of local outbreak for each age group.
+        Probability of major outbreak for each age group.
     beta : numpy.ndarray
         Beta parameter.
 
@@ -72,7 +72,7 @@ def system_of_equations(q, beta):
     numpy.ndarray
         System of equations.
     """
-    n = len(q)  # Number of q_i
+    n = len(q) # Number of q_i
     equations = np.zeros(n)
 
     for i in range(n):
@@ -92,12 +92,12 @@ def system_of_equations(q, beta):
 
 def prob_outbreak(year, r0=2):
     """
-    Returns the probability of a local outbreak given an index case in each age group.
+    Returns the probability of a major outbreak given an index case in each age group.
     
     Parameters
     -----------
     year : int
-        Year for which the probability of a local outbreak is to be calculated.
+        Year for which the probability of a major outbreak is to be calculated.
     r0 : float
         Basic reproduction number.
 
@@ -105,9 +105,11 @@ def prob_outbreak(year, r0=2):
     --------
     tuple
         - p : numpy.ndarray
-            Probability of local outbreak for each age group.
+            Probability of major outbreak for each age group.
         - PLO : float
-            Average outbreak probability.
+            Population average outbreak probability.
+        - tc_p : float
+            Contact-weighted average outbreak probability.
     """
     pop_data = pd.read_csv('Data/Nigeria_population.csv', header=0, dtype={'Data': str, 'Population': float})
     pop_data.set_index('year', inplace=True)
@@ -129,16 +131,25 @@ def prob_outbreak(year, r0=2):
     # Solve the system of equations
     solution = fsolve(system_of_equations, initial_guess, args=(Beta,))
     p = 1 - solution
-    return p, np.sum(p * N_prop)
 
-p, PLO = prob_outbreak(2025, 2)
+    # total contacts for age group i
+    tc = np.zeros(16)
+    for i in range(16):
+        tc[i] += C[i, :].sum()
+
+    return p, np.sum(p * N_prop), np.sum(tc*pop*p)/np.sum(tc*pop)
+
+# Figure 5B-D
+
+p, PLO, tc_p = prob_outbreak(2000, 2) # Can change year to 2000, 2025, or 2050
 
 fig = plt.gcf()
 fig.set_size_inches(8, 6)
-plt.bar(range(1, 17), p, color='lightblue', edgecolor='black', linewidth=1)
-plt.axhline(y=PLO, color='black', linewidth=2, label='M2')
+plt.bar(range(1, 17), p, color='#ffc067', edgecolor='black', linewidth=1)
+plt.axhline(y=PLO, color='black', linewidth=2)
+plt.axhline(y=tc_p, color='red', linewidth=2, linestyle='--')
 plt.xlabel('Age group of index case', labelpad=10, fontsize=20)
-plt.ylabel(r'Probability of local outbreak ($p_k$)', labelpad=10, fontsize=20)
+plt.ylabel('Major outbreak probability', labelpad=10, fontsize=20)
 plt.xticks(np.arange(1, 17), ['0-4', '5-9', '10-14', '15-19', '20-24', '25-29', '30-34', '35-39',
                               '40-44', '45-49', '50-54', '55-59', '60-64', '65-69', '70-74', '75+'],
                               rotation=45)
@@ -150,5 +161,46 @@ plt.tight_layout()
 ax = plt.gca()
 ax.spines['top'].set_visible(False)
 ax.spines['right'].set_visible(False)
-plt.savefig('Figures/Outbreak_probability_nigeria_2025.svg', bbox_inches='tight')
+# plt.savefig('Figures/Outbreak_probability_nigeria_2000.svg', bbox_inches='tight')
+plt.show()
+
+# Figure 5A
+
+data = pd.read_csv('Data/Nigeria_population.csv', header=0, dtype={'Data': str, 'Population': float})
+df = pd.DataFrame(data)
+# Set year as index
+df.set_index('year', inplace=True)
+
+# Convert counts to proportions (row-wise)
+df_prop = df.div(df.sum(axis=1), axis=0)
+
+colors = [plt.cm.Blues(i) for i in np.linspace(0.2, 1, 4)] + \
+         [plt.cm.Reds(i) for i in np.linspace(0.2, 1, 6)] + \
+         [plt.cm.Greens(i) for i in np.linspace(0.2, 1, 6)]
+
+df_prop.plot(
+    kind='bar',
+    stacked=True,
+    color=colors,
+    figsize=(6, 6),
+    width=0.8
+)
+
+fig = plt.gcf()
+fig.set_size_inches(8, 6)
+plt.xticks(rotation=0, fontsize=18)
+plt.yticks(fontsize=18)
+plt.ylabel('Proportion of population', labelpad=10, fontsize=20)
+plt.xlabel('Year', labelpad=10, fontsize=20)
+plt.legend(title='Age group', bbox_to_anchor=(1.05, 1), loc='upper left', fontsize=14, title_fontsize=14)
+plt.ylim(0, 1)
+# add total population on top of each bar
+totals = df.sum(axis=1)
+for i, total in enumerate(totals):
+    plt.text(i, 1.02, f'{total:,}', ha='center', va='bottom', fontsize=14)
+plt.tight_layout()
+ax = plt.gca()
+ax.spines['top'].set_visible(False)
+ax.spines['right'].set_visible(False)
+# plt.savefig('Figures/Proportions_korea.svg', bbox_inches='tight')
 plt.show()
