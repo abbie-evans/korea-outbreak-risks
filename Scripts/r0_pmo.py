a723da61d85eebe50cd77328564d01f7bebdec14
Abbie Evans <147088150+abbie-evans@users.noreply.github.com>
2026-08-14 16:28:41 +0100

    Update to negative binomial

--- Scripts/r0_pmo.py
+++ Scripts/r0_pmo.py
@@ -56,7 +56,7 @@ def calculate_contact_matrix(contact_matrix, population):
 
     return C_
 
-def system_of_equations(q, beta):
+def system_of_equations(q, beta, k=1):
     """
     System of equations for the major outbreak probability.
 
@@ -66,31 +66,27 @@ def system_of_equations(q, beta):
         Probability of major outbreak for each age group.
     beta : numpy.ndarray
         Beta parameter.
+    k : float
+        Dispersion parameter.
 
     Returns
     --------
     numpy.ndarray
         System of equations.
     """
-    n = len(q) # Number of q_i
+    n = len(q)
     equations = np.zeros(n)
 
     for i in range(n):
-        # Compute the sum in the first term of the equation
-        sum_beta_ik = np.sum(beta[i, :])  # sum for k=1 to 16
-        first_term = 1 / (1 + sum_beta_ik)
-
-        # Compute the sum in the second term of the equation
-        second_term = 0
+        first_term = 0.0
         for j in range(n):
-            second_term += (beta[i, j] / (1 + sum_beta_ik)) * q[i] * q[j]
-        
-        # Equation for q_i
-        equations[i] = q[i] - (first_term + second_term)
-    
+            first_term += beta[i, j] * (1 - q[j])
+
+        equations[i] = q[i] - (1 + first_term / k) ** (-k)
+
     return equations
 
-def prob_outbreak(year, r0=2):
+def prob_outbreak(year, r0=2, k=1):
     """
     Returns the probability of a major outbreak given an index case in each age group.
     
@@ -100,6 +96,8 @@ def prob_outbreak(year, r0=2):
         Year for which the probability of a major outbreak is to be calculated.
     r0 : float
         Basic reproduction number.
+    k : float
+        Dispersion parameter.
 
     Returns
     --------
@@ -127,7 +125,7 @@ def prob_outbreak(year, r0=2):
     initial_guess = np.ones(16)*0.3
 
     # Solve the system of equations
-    solution = fsolve(system_of_equations, initial_guess, args=(Beta,))
+    solution = fsolve(system_of_equations, initial_guess, args=(Beta,k))
     p = 1 - solution
 
     return p, np.sum(p * N_prop)
@@ -139,11 +137,11 @@ P_2025 = []
 P_2000 = []
 P_2050 = []
 for r0 in np.arange(1, 6, 0.01):
-    p, PLO = prob_outbreak(2025, r0)
+    p, PLO = prob_outbreak(2025, r0, k=1)
     P_2025.append(PLO)
-    p, PLO = prob_outbreak(2000, r0)
+    p, PLO = prob_outbreak(2000, r0, k=1)
     P_2000.append(PLO)
-    p, PLO = prob_outbreak(2050, r0)
+    p, PLO = prob_outbreak(2050, r0, k=1)
     P_2050.append(PLO)
 plt.plot(np.arange(1, 6, 0.01), P_2000, '-', color='black', lw=2, label='2000')
 plt.plot(np.arange(1, 6, 0.01), P_2025, '-', color='blue', lw=2, label='2025')
